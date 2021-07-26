93e51004c8147e99f0e6c515b4a316daa5987408
Cameron <camcreel@gmail.com>
2021-07-25 20:51:50 -0400

    Completed assignment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,27 @@
+# ENV["RACK_ENV"] ||= "development"
+
+# require 'rake'
+# require 'active_record'
+# require 'yaml/store'
+# require 'ostruct'
+# require 'date'
+
+
+# require 'bundler/setup'
+# Bundler.require(:default, ENV["RACK_ENV"])
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/artists.sqlite"
+# ) 
+
+# require_relative "../app/models/artist"
+
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/artist"
+
+
